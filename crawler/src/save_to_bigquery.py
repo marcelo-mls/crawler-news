bef768aaaf46694db3b0b7ac16d683a59b9718a5
marcelo marques <marcelo-mls@hotmail.com>
2023-05-24 23:18:35 -0300

    feat(crawler): creates logic to add data to a BigQuery table

--- crawler/src/save_to_bigquery.py
+++ crawler/src/save_to_bigquery.py
@@ -9,3 +9,43 @@ def save_to_bigquery(data, dataset_id, table_id):
     client = bigquery.Client(credentials=credentials)
     print("\nBigQuery: Connection established with BigQuery")
     print("BigQuery: Connection closed\n")
+
+    # Create the dataset if it doesn't exist
+    dataset_ref = client.dataset(dataset_id)
+    dataset = bigquery.Dataset(dataset_ref)
+    dataset.location = "US"
+
+    try:
+        client.create_dataset(dataset)
+        print("BigQuery: Dataset created:", dataset_id)
+    except Exception as error:
+        print("BigQuery: Error creating dataset:", error)
+
+    # Create the table if it doesn't exist
+    table_ref = dataset_ref.table(table_id)
+    table = bigquery.Table(table_ref)
+
+    table.schema = [
+        bigquery.SchemaField("url", "STRING"),
+        bigquery.SchemaField("headline", "STRING"),
+        bigquery.SchemaField("author", "STRING"),
+        bigquery.SchemaField("subtitle", "STRING"),
+        bigquery.SchemaField("text", "STRING"),
+        bigquery.SchemaField("timestamp", "TIMESTAMP"),
+    ]
+
+    try:
+        table = client.create_table(table, exists_ok=True)
+        print("BigQuery: Table created:", table_id)
+    except Exception as error:
+        print("BigQuery: Error creating table:", error)
+
+    # Insert data into the table
+    errors = client.insert_rows_json(table, data)
+
+    if errors:
+        print("BigQuery: Error inserting rows:", errors)
+    else:
+        print("BigQuery: Data inserted into BigQuery")
+
+    print("BigQuery: Connection closed\n")
