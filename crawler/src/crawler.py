8ccb5c03b836015a5fad9324b01ac8650cf173aa
marcelo marques <marcelo-mls@hotmail.com>
2023-05-24 22:39:11 -0300

    feat(crawler): add a function that returns a list of BBC article URLs

--- crawler/src/crawler.py
+++ crawler/src/crawler.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from parsel import Selector
 
@@ -16,3 +17,20 @@ def get_parsed_selector(url):
                 "url": response.url,
                 "selector": Selector(text=response.text),
             }
+
+
+def get_bbc_news_endpoints():
+    selector = get_parsed_selector("https://www.bbc.com/news")["selector"]
+
+    news_urls = selector.css("a.gs-c-promo-heading::attr(href)").getall()
+    regex_pattern = r"/news/[a-z-]+\d+"
+
+    # Filter only news URLs and eliminate possible duplicate URLs
+    filtered_news = list(
+        set(
+            "https://www.bbc.com" + news
+            for news in news_urls
+            if re.match(regex_pattern, news)
+        )
+    )
+    return filtered_news
