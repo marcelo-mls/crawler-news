d7a87f8f0ee2a1492f11b0124ad2253a948eda90
marcelo marques <marcelo-mls@hotmail.com>
2023-05-25 00:39:49 -0300

    feat(api): establishes the connection with bigquery to make the SQL query by keyword

---
+++ api/src/models/articles.model.js
@@ -0,0 +1,26 @@
+const { BigQuery } = require('@google-cloud/bigquery');
+const path = require('path');
+
+// Configure Google Cloud BigQuery with the service account key file path
+const currentDir = path.dirname(__filename);
+const credentialsPath = path.join(currentDir, '..', '..', '..', 'crawler', 'src', 'credentials', 'sa_gbq_crawler_credentials.json');
+
+const bigquery = new BigQuery({ keyFilename: credentialsPath });
+
+async function searchArticlesByKeywords(keywords) {
+  const query = `
+    SELECT *
+    FROM lima-technical-challenge.crawler.bbc_news
+    WHERE LOWER(headline) LIKE '%${keywords.toLowerCase()}%'
+      OR LOWER(subtitle) LIKE '%${keywords.toLowerCase()}%'
+      OR LOWER(text) LIKE '%${keywords.toLowerCase()}%'
+  `;
+
+  const [rows] = await bigquery.query(query);
+  console.log(rows)
+  return rows;
+}
+
+module.exports = {
+  searchArticlesByKeywords,
+}
